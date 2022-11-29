cadd17f667b09f95b475d7408c2fa5570ed0bbf2
Yann Lecocq <mandza@hotmail.fr>
2022-11-29 10:20:28 +0100

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "creating pratician"
+
+Yann Lecocq = [first_name: "Yann", last_name: "Lecocq", job: "Hypnose Ericksonienne", specialty: "Gestion des émotions", phone_number: "0609821604", email: "yannlecocqhypnose44@gmail.com", address: "62 Bd Jean Ingres 44300 St Herblain"]
+Edith Wernert = [first_name: "Edith", last_name: "Wernert", job: "Hypnologue", specialty: "", phone_number: "07 64 48 22 11", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+ = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+Virginie Azeau = [first_name: "Virginie", last_name: "Azeau", job: "Soin énergétique", specialty: "", phone_number: "", email: "", address: ""]
+#  = [first_name: "", last_name: "", job: "", specialty: "", phone_number: "", email: "", address: ""]
+
+puts "creating pratician done"
