a4ed08231e4f77bf1d4f174c5e7cb75fe6ab6a1f
Sam Mano <sammano44@gmail.com>
2022-11-29 17:07:49 +0100

    maj2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,2 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    @user.acceptation = "pending"
-    @user.user = curuser_user
-  end
-
-  def update
-    @user = User.find(params[:id])
-    @user.update(params[:user]) # Will raise ActiveModel::ForbiddenAttributesError
-  end
-
 end
