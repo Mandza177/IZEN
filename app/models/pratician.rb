d7265998c2e090fb304c47c891a997ef036edf71
Yann Lecocq <mandza@hotmail.fr>
2022-11-30 15:34:11 +0100

    favorite , praticians et seeds

--- app/models/pratician.rb
+++ app/models/pratician.rb
@@ -1,7 +1,6 @@
 class Pratician < ApplicationRecord
   has_many :users, through: :favorites
   has_one :symptom
-  
 
   validates :first_name, presence: true
   validates :last_name, presence: true
