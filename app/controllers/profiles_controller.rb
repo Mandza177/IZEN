2faba700a404bd49c125cbf38df7eddf4735470e
Yann Lecocq <mandza@hotmail.fr>
2022-11-30 16:44:58 +0100

    mAJ controller profiles create et update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,19 @@
 class ProfilesController < ApplicationController
+
+  def create
+    @profile = Profile.new(params [:profile])
+    @profile.save
+    redirect_to profile_path(@profile)
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    @profile.update(params[:profile])
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:result_test, :symptom_id)
+  end
 end
