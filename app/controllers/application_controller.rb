9836194dfef73e88198eea885e02baa0f661c391
arnaud thiebart <arno.thieb@gmail.com>
2022-12-02 11:11:12 +0100

    fontsbootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  # before_action :authenticate_user!
-  
-=======
   # before_action :authenticate_user!, only: [ :index ]
->>>>>>> ed3b6e30fee69b1c5abca6be731c7e0be24dc733
 end
