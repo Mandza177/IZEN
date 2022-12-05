bb8d10fe44642eeffb1bf0f3e7e200fd0fcdb849
Pascal Billerach <p.billerach@gmail.com>
2022-12-05 12:57:17 +0100

    pages métiers en cours

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,12 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home, :index, :show ]
 
   def home
-  
+  end
+
+  def thematiques
+  end
+
+  def hypnose
   end
 
   def profile
