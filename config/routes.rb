6c80f3f9aa83f2837e5fcbe83ceb80fc7b857a49
Sam Mano <sammano44@gmail.com>
2022-12-05 12:54:23 +0100

    new

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "/profile", to: "pages#profile"
-
+  get "/pratiques/hynopse",       to: "pages#hypnose"
+  get "/pratiques/naturopathie",  to: "pages#naturopathie"
+  get "/pratiques/osteopathie",   to: "pages#osteopathie"
+  get "/pratiques/chiropratique", to: "pages#chiropratique"
+  get "/pratiques/sonotherapie",  to: "pages#sonotherapie"
+  get "/thematique",  to: "pages#thematique"
+  
   resources :users, only: [:create, :update]
   resources :praticians, only: [:index, :show]
 end
