2e41941bf3f3b9952a87ca57dbc31d4b526939e8
Pascal Billerach <p.billerach@gmail.com>
2022-12-05 17:45:12 +0100

    conflits de merging de fin de journée résolus

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,16 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "/profile",       to: "pages#profile"
-
-<<<<<<< HEAD
-=======
-
->>>>>>> master
   get "/hypnose",       to: "pages#hypnose"
   get "/naturopathie",  to: "pages#naturopathie"
   get "/ostheopathie",   to: "pages#ostheopathie"
   get "/chiropratique", to: "pages#chiropratique"
   get "/sonotherapie",  to: "pages#sonotherapie"
 
-<<<<<<< HEAD
   get "/thematique1",  to: "pages#thematique1"
   get "/thematique2",  to: "pages#thematique2"
   get "/thematique3",  to: "pages#thematique3"
-=======
-  get "/thematique",  to: "pages#thematique"
->>>>>>> master
+
 
   resources :users, only: [:create, :update, :destroy]
   resources :praticians, only: [:index, :show]
