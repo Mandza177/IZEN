559ee5711f51b3aa3f37984bfb59a03586311ea0
Pascal Billerach <p.billerach@gmail.com>
2022-12-06 15:44:30 +0100

    remplacement des @@ par des ILIKE dans le pratician ctrler (requete SQL)

--- app/controllers/praticians_controller.rb
+++ app/controllers/praticians_controller.rb
@@ -3,29 +3,25 @@ class PraticiansController < ApplicationController
   def index
 
     if params[:query].present? && params[:address].present?
-        sql_query = <<~SQL
-        praticians.first_name @@ :query
-          OR praticians.job @@ :query
-          OR praticians.last_name @@ :query
-          OR symptoms.description @@ :query
-        SQL
-      @praticians = Pratician.joins(:symptom).where("praticians.address @@ :address", address: params[:address]).where(sql_query, query: "%#{params[:query]}%")
+      sql_query = <<~SQL
+        praticians.first_name ILIKE :query
+        OR praticians.job ILIKE :query
+        OR praticians.last_name ILIKE :query
+        OR symptoms.description ILIKE :query
+      SQL
+      @praticians = Pratician.joins(:symptom).where("praticians.address ILIKE :address", address: params[:address]).where(sql_query, query: "%#{params[:query]}%")
+    elsif params[:query].present? && params[:address].blank?
+      sql_query = <<~SQL
+        praticians.first_name ILIKE :query
+        OR praticians.job ILIKE :query
+        OR praticians.last_name ILIKE :query
+        OR symptoms.description ILIKE :query
+      SQL
+      @praticians = Pratician.joins(:symptom).where(sql_query, query: "%#{params[:query]}%")
+    elsif params[:address].present?
+      @praticians = Pratician.where("praticians.address ILIKE :address", address: params[:address])
     else
-      if params[:query].present? && params[:address].blank?
-        sql_query = <<~SQL
-          praticians.first_name @@ :query
-          OR praticians.job @@ :query
-          OR praticians.last_name @@ :query
-          OR symptoms.description @@ :query
-        SQL
-        @praticians = Pratician.joins(:symptom).where(sql_query, query: "%#{params[:query]}%")
-      else
-        if params[:address].present?
-          @praticians = Pratician.where("praticians.address @@ :address", address: params[:address])
-        else
-          @praticians = Pratician.all
-        end
-      end
+      @praticians = Pratician.all
     end
   end
 
