853ebecb22353e3221dbe8791f85a33b11667420
Yann Lecocq <mandza@hotmail.fr>
2022-12-07 12:02:55 +0100

    back du questionnaire

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,40 +5,33 @@ class ProfilesController < ApplicationController
 
   def update
 
-    # redirect_to profile_path
-    
-    params[:choice][choice_1][0]
-    params[:choice][choice_2][0]
-    params[:choice][choice_3][0]
-    params[:choice][choice_4][0]
+    # params[:choice]["choice_1"][0]
+    # params[:choice]["choice_2"][0]
+    # params[:choice]["choice_3"][0]
+    # params[:choice]["choice_4"][0]
+    if params[:choice]["choice_1"].first == "a" && params[:choice]["choice_2"].first == "a" && params[:choice]["choice_3"].first == "a" && params[:choice]["choice_4"].first == "a"
+      current_user.result_test = 1
+    end
 
-    current_user.result_test = "Auditif"
+    # params[:choice][choice_1][1]
+    # params[:choice][choice_2][1]
+    # params[:choice][choice_3][1]
+    # params[:choice][choice_4][1]
+    if params[:choice]["choice_1"].first == "b" && params[:choice]["choice_2"].first == "b" && params[:choice]["choice_3"].first == "b" && params[:choice]["choice_4"].first == "b"
+      current_user.result_test = 2
+    end
 
-    params[:choice][choice_1][1]
-    params[:choice][choice_2][1]
-    params[:choice][choice_3][1]
-    params[:choice][choice_4][1]
-    current_user.result_test = "Corporel"
-
-    params[:choice][choice_1][2]
-    params[:choice][choice_2][2]
-    params[:choice][choice_3][2]
-    params[:choice][choice_4][2]
-    current_user.result_test = "Visuel"
-
-
+    # params[:choice][choice_1][2]
+    # params[:choice][choice_2][2]
+    # params[:choice][choice_3][2]
+    # params[:choice][choice_4][2]
+    if params[:choice]["choice_1"].first == "c" && params[:choice]["choice_2"].first == "c" && params[:choice]["choice_3"].first == "c" && params[:choice]["choice_4"].first == "c"
+      current_user.result_test = 1
+    end
+    redirect_to profile_path
   end
 
   def show
     @favorite_praticians = current_user.praticians
   end
-
-  # private
-
-  # def survey_algo
-  #   params.require(:choice).permit(:choice_1)["choice_1"] # "A: Entendre le chant des vagues"
-  #   params.require(:choice).permit(:choice_2)["choice_1"] # "A: Je procède par étape pour le résoudre"
-  #   params.require(:choice).permit(:choice_3)["choice_1"] # "A: Je crie et je discute après"
-  #   params.require(:choice).permit(:choice_4)["choice_1"] # "B: Un massage à 4 mains"
-  # end
 end
